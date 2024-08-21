8889a90f1c2d411eec83b27bbc199e9beefd62cd
Oopss <lequangsang.work@gmail.com>
2024-08-21 20:49:48 +0700

    Add project files.

---
+++ WebSellingPhone.Data/Models/OrderDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace WebSellingPhone.Data.Models
+{
+    public class OrderDetail
+    {
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Products { get; set; }
+
+        public Guid OrderId { get; set; }   
+        public Order Order { get; set; }
+    }
+}
