c527e6dd5f5de6e67792458544736a82454b05de
Oopss <lequangsang.work@gmail.com>
2024-08-22 10:15:03 +0700

    add service interface

---
+++ WebSellingPhone.Bussiness/Service/IReviewService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSellingPhone.Bussiness.Service.Base;
+using WebSellingPhone.Bussiness.ViewModel;
+using WebSellingPhone.Data.Models;
+
+namespace WebSellingPhone.Bussiness.Service
+{
+    public interface IReviewService : IBaseService<Review>
+    {
+        Task<PaginatedResult<Review>> GetByPagingAsync(
+            string filter = "",
+            string sortBy = "",
+            int pageIndex = 1,
+            int pageSize = 10);
+    }
+}
