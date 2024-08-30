45a15342ec453c542144e61bf1359a627a17db77
bruh3112 <maivanhoanganh3112@gmail.com>
2024-08-30 20:14:18 +0700

    Update OrderController.cs

--- WebSellingPhone.WebAPI/Controllers/OrderController.cs
+++ WebSellingPhone.WebAPI/Controllers/OrderController.cs
@@ -35,7 +35,27 @@ namespace WebSellingPhone.WebAPI.Controllers
         }
 
 
-        
+        [HttpGet("get-orders-by-userId/{userId}")]
+        public async Task<IActionResult> GetOrdersByUserId(Guid userId)
+        {
+            var orders = await _orderService.GetOrdersByUserIdAsync(userId);
+
+            if (orders == null || !orders.Any())
+            {
+                return NotFound();
+            }
+
+            var ordersViewModels = orders.Select(q => new OrderVm()
+            {
+                Id = q.Id,
+                TotalAmount = q.TotalAmount,
+                PaymentMethod = q.PaymentMethod,
+                UserOrderId = q.UserOrderId,
+            }).ToList();
+
+            return Ok(ordersViewModels);
+        }
+
         [HttpGet("get-order/{id}")]
         public async Task<IActionResult> GetOrderById(Guid id)
         {
@@ -113,27 +133,6 @@ namespace WebSellingPhone.WebAPI.Controllers
 
             await _orderService.DeleteAsync(order);
             return NoContent();
-        }
-
-        [HttpGet("get-orders-by-userId/{userId}")]
-        public async Task<IActionResult> GetOrdersByUserId(Guid userId)
-        {
-            var orders = await _orderService.GetOrdersByUserIdAsync(userId);
-
-            if (orders == null || !orders.Any())
-            {
-                return NotFound();
-            }
-
-            var ordersViewModels = orders.Select(q => new OrderVm()
-            {
-                Id = q.Id,
-                TotalAmount = q.TotalAmount,
-                PaymentMethod = q.PaymentMethod,
-                UserOrderId = q.UserOrderId,
-            }).ToList();
-
-            return Ok(ordersViewModels);
         }
 
     }
